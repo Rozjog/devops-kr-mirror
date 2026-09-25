506097d0c9c8634578b6b345103176566c9132b8
Rozjog <muratilya@mail.ru>
2026-09-25 14:29:26 +0300

    feat: add phone validation function

---
+++ tests/test_validator.py
@@ -0,0 +1,6 @@
+from validator import validate_phone
+
+def test_validate_phone():
+    assert validate_phone("+79991234567") == True
+    assert validate_phone("89991234567") == False
+    assert validate_phone("+7999123") == False
